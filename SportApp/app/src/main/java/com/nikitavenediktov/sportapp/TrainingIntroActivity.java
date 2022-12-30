c9a0cf5be5833ad6ef482d240d088f2df57ca2e6
NikitaVen <nikben2105@gmail.com>
2022-12-30 19:50:40 +0300

    Exercise pipeline added, fixed bugs with orientation

--- SportApp/app/src/main/java/com/nikitavenediktov/sportapp/TrainingIntroActivity.java
+++ SportApp/app/src/main/java/com/nikitavenediktov/sportapp/TrainingIntroActivity.java
@@ -1,7 +1,9 @@
 package com.nikitavenediktov.sportapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class TrainingIntroActivity extends AppCompatActivity {
+public class TrainingIntroActivity extends AppCompatActivity implements View.OnClickListener{
     private String training_id;
     private ArrayList<Exercise> exercises;
     private TrainingIntroAdapter adapter;
@@ -39,5 +41,17 @@ public class TrainingIntroActivity extends AppCompatActivity {
 
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(TrainingIntroActivity.this));
+
+        start_btn.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(final View v) {
+
+        Intent intent = new Intent(TrainingIntroActivity.this, ExercisesPipelineActivity.class);
+
+        intent.putExtra("training_id", training_id);
+        startActivity(intent);
+    }
+
 }
