e10e4633a7f25f84dba4cd23fd4e75c9838aa49a
NikitaVen <nikben2105@gmail.com>
2023-01-03 00:49:36 +0300

    Added: share button logic, application icon, daily notifications

--- SportApp/app/src/main/java/com/nikitavenediktov/sportapp/MainActivity.java
+++ SportApp/app/src/main/java/com/nikitavenediktov/sportapp/MainActivity.java
@@ -1,10 +1,18 @@
 package com.nikitavenediktov.sportapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
@@ -12,6 +20,7 @@ import com.nikitavenediktov.sportapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String NOTIFICATION_CHANNEL_ID = "notifySport";
     private ActivityMainBinding binding;
 
     @Override
@@ -20,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        createNotificationChannel();
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager
@@ -31,5 +40,29 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(pagerAdapter);
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
+
+        // for notifications
+        Intent intent = new Intent(MainActivity.this, ReminderBroadcast.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP,  System.currentTimeMillis() + 24 * 60 * 60 * 1000, pendingIntent);
     }
+
+    private void createNotificationChannel()
+    {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            CharSequence name = "SportReminderChannel";
+            String description = "Channel for Training reminder";
+
+            NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, name,
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 }
--- SportApp/app/src/main/java/com/nikitavenediktov/sportapp/TrainingOverActivity.java
+++ SportApp/app/src/main/java/com/nikitavenediktov/sportapp/TrainingOverActivity.java
@@ -2,12 +2,18 @@ package com.nikitavenediktov.sportapp;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class TrainingOverActivity extends AppCompatActivity implements View.OnClickListener{
     private String training_id;
@@ -40,5 +46,44 @@ public class TrainingOverActivity extends AppCompatActivity implements View.OnCl
     @Override
     public void onClick(View v) {
         // share logic
+        @SuppressLint("UseCompatLoadingForDrawables")
+        BitmapDrawable drawable = (BitmapDrawable) getDrawable(R.drawable.sport);
+        Bitmap bitmap = drawable.getBitmap();
+        String message = getShareMessage();
+
+        String bitmapPath = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap,
+                "logo", null);
+
+        Uri uri = Uri.parse(bitmapPath);
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("image/png");
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        startActivity(Intent.createChooser(intent, getString(R.string.share_via)));
+    }
+
+    @SuppressLint("DefaultLocale")
+    private String getShareMessage()
+    {
+        ArrayList<Exercise> exercises = SportDbHelper.getInstance(this)
+                .getTrainingExercises(Integer.parseInt(training_id));
+
+        StringBuilder message = new StringBuilder(getString(R.string.share_msg_start));
+        for(Exercise exercise : exercises)
+        {
+            message.append("\n - ").append(getString(getResources().getIdentifier(exercise.title, "string", getPackageName())));
+            message.append(" (");
+            if (exercise.description.contains("s"))
+                message.append(exercise.description.subSequence(0, exercise.description.length() - 1))
+                        .append(' ').append(getString(R.string.sec)).append(')');
+            else
+                message.append(exercise.description).append(')');
+        }
+        message.append('\n').append(getString(R.string.share_msg_time)).append(String.format(" %d %s %d %s.",
+                total_time / 60, getString(R.string.min), total_time % 60, getString(R.string.sec)));
+        message.append('\n').append(getString(R.string.share_msg_end)).append(' ').append(getString(R.string.app_name)).append('.');
+
+        return message.toString();
     }
 }
